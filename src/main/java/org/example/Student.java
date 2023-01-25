fa93baf28bed5de2e362db4cf726ed24a3daf467
YaswanthVarma754 <konduru.varma@tringapps.com>
2023-01-25 16:28:30 +0530

    changed variable names

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class Student {
     private String name;
-    private char grade_level;
+    private char gradeLevel;
     private double gpa;
 
-    Student(String name, char grade_level, double gpa){
+    Student(String name, char gradeLevel, double gpa){
         this.name = name;
-        this.grade_level = grade_level;
+        this.gradeLevel = gradeLevel;
         this.gpa = gpa;
     }
 
-    public void update_GPA(double new_gpa){
-        gpa = new_gpa;
+    public void updateGPA(double newGpa){
+        gpa = newGpa;
     }
 
     public String student_grade(){
@@ -26,18 +26,18 @@ class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name;
-        char grade_level;
+        char gradeLevel;
         double gpa;
         System.out.println("Enter name of the student : ");
         name = sc.nextLine();
         System.out.println("Enter the grade level : ");
-        grade_level = sc.next().charAt(0);
+        gradeLevel = sc.next().charAt(0);
         System.out.println("Enter GPA : ");
         gpa = sc.nextDouble();
-        Student s = new Student(name, grade_level, gpa);
+        Student s = new Student(name, gradeLevel, gpa);
         String studentGrade = s.student_grade();
         System.out.println(studentGrade);
-        s.update_GPA(94.6);
+        s.updateGPA(94.6);
         studentGrade = s.student_grade();
         System.out.println(studentGrade);
     }
